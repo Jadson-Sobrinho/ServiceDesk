9072b8ed40694685ebd7bcaad2b23e29ff17e790
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-08-16 15:52:48 -0300

    Chore - Alteração da porta para não conflitar com o Next

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 const app = require("./app");
 const ConnectToDataBase = require("./api/config/db");
 
-const PORT = 3000;
+const PORT = 3001;
 
 async function startServer() {
     await ConnectToDataBase();
 
     app.listen(PORT, () => {
-        console.log(`Servidor Rodando em https://localhost:${PORT}`);
+        console.log(`Servidor Rodando em http://localhost:${PORT}`);
     });
 }
 
