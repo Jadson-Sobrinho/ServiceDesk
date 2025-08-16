a79289bb457d8f0cbec6ccf909a5b7bd80e22310
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-08-16 15:52:06 -0300

    Chore - Lib CORS para o front poder fazer requisição para o back

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const ticketRoute = require("./api/routes/ticket");
+
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
+
+app.use(express.json());
 
 app.use('/ticket', ticketRoute);
 
