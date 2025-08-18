6a60568574192c838eb0fb0600314c901861e92f
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-08-18 20:31:41 -0300

    Feat - Rota para buscar tickets do usuário logado

--- backend/api/routes/ticket.js
+++ backend/api/routes/ticket.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const ticketController = require("../controllers/ticket");
+const auth = require("../controllers/auth");
 
 
 router.get('/', ticketController.getAllTickets);
 
 router.post('/', ticketController.createTicket);
+
+router.get('/user', auth.verifyToken, ticketController.getUserTickets);
 
 module.exports = router;
