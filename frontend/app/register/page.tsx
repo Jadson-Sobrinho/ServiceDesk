466ff8186d408ccff9ea174ad719b0ace933fa36
Jadson Sobrinho <jadsonsilva34677@gmail.com>
2025-09-05 09:36:10 -0300

    Chore - Configuração para subir na nuvem

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -75,7 +75,7 @@ export default function RegisterPage() {
       console.log("Registration data:", formData)
       const {repeatPassword, ...dataToSend} = formData;
         try {
-          const response = await fetch("http://localhost:3001/register", {
+          const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "/register", {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
